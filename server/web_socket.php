ab16b19265246be3e26ca1bad7d70a87b4b51c0e
chenzhi <13509622742@163.com>
2019-02-15 18:52:07 +0800

    1111

--- server/web_socket.php
+++ server/web_socket.php
@@ -30,9 +30,12 @@ $server->on('request', function ($request, $response) {
     global $server;  //调用方法体外部server
 
     //$server->connections 遍历所有websocket连接用户的fd，给所有用户推送
+    $fds = '';
     foreach ($server->connections as $fd) {
-        $server->push($fd, $request->get['message']);
+        $server->push($fd, $request->get['message'].$fd);
+        $fds .= '&'.$fd;
     }
+    $response->end($fds.' ********** '. json_encode($request->get));
 });
 
 $server->start();
